054ffce5aed7d9685d4d7b3aa4c199b2a6af5b02
paulbipradeep <138135641+paulbipradeep@users.noreply.github.com>
2025-04-13 19:58:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var typed = new Typed(".typing",{
 
 const nav = document.querySelector(".nav"),
       navList = nav.querySelectorAll("li"),
-      totalNavList = navList.length,
+      totalNavList = navList.length;
       allSection = document.querySelectorAll(".section"),
       totalSection = allSection.length;
       for(let i=0; i<totalNavList; i++)
